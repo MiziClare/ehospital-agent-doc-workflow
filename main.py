ad826d5df04ab8e768a7e184ea996453fcbdeba4
yqy <1749288146yqy@gmail.com>
2025-11-24 23:54:17 -0500

    fastapi middleware settings, and adding a main function to start service quickly

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
+import uvicorn
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
 from app.routers import router
 
 app = FastAPI(title="eHealth API - Create/Get")
-
+# 添加CORS中间件
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(router, prefix="/api")
 
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True
+    )
